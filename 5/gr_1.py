e33f8f87292232e321006d82dde8e87beee09d25
zakharov-artem-coder <szakharovartem@gmail.com>
2025-01-16 19:24:18 +0100

    lesson 5

---
+++ 5/gr_1.py
@@ -0,0 +1,21 @@
+# Вивести числа від 0 до 10 в консоль
+# count = 0
+# while count <= 10:
+#   print(count)
+#   count += 1
+
+# Знаходження максимального числа в списку
+# numbers = [1, 2, 3, 4, 5]
+
+# maxNumber = numbers[0]
+# for i in range(1, len(numbers)):
+#   if numbers[i] > maxNumber:
+#     maxNumber = numbers[i]
+
+# print(maxNumber, max(numbers))
+
+# Вивести таблицю множення в консоль
+# for i in range(1, 11):
+#   for j in range(1, 11):
+#     print(f"{i} x {j} = {i * j}")
+#   print("\n")
---
+++ 5/gr_2.py
@@ -0,0 +1,20 @@
+# Чищення зубів
+# from time import sleep
+# from random import randint
+
+# number = 1
+# while number <= 32:
+#   print(number)
+#   sleep(0.3 + randint(1, 10) * 0.1)
+#   number += 1
+
+# Вивести числа на екран 0, 5, 10, 15, 20 ... 100
+# for i in range(0, 101, 5):
+#   print(i)
+
+# Вивести пірамідки
+# for i in range(1, 20, 2):
+#   print("*" * i)
+
+# for i in range(10):
+#   print(" " * (10 - i - 1), "*" * (2 * i - 1))

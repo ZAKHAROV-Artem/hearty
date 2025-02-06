6f8941f595decaacd09af96188ecba741c2209af
zakharov-artem-coder <szakharovartem@gmail.com>
2025-02-06 19:26:12 +0100

    lesson 6

---
+++ 6/gr_1.py
@@ -0,0 +1,57 @@
+# def checkPincode(pincode):
+#   if pincode == 1111:
+#     print("pincode is correct")
+#   else:
+#     print("pincode is not correct")
+
+# checkPincode(2354)
+
+
+# def power2(x):
+#   return x * x
+
+
+# number = power2(4)
+# print(number)
+
+
+# def add(num1, num2):
+#   return num1 + num2
+
+
+# def subtract(num1, num2):
+#   return num1 - num2
+
+
+# result = add(subtract(10, 2), 10)
+# print(result)
+
+
+# def isAdult(age):
+#   if age >= 18:
+#     return True
+#   else:
+#     return False
+
+
+# def getAge():
+#   return int(input("Enter your age: "))
+
+
+# result = isAdult(getAge())
+# if result:
+#   print("You can vote !")
+# else:
+#   print("You are child go to school")
+
+
+# Написати функцію, яка виводить числа від 0 до 100 включно в консолі
+# def printFrom0To100():
+#   for i in range(0, 101):
+#     print(i)
+
+
+# printFrom0To100()
+
+# ДЗ Створіть функцію, яка приймає ім'я та вік,
+# а потім виводить повідомлення: "Привіт, [ім'я]! Тобі [вік] років."
---
+++ 6/gr_2.py
@@ -0,0 +1,69 @@
+# print("1")
+# print("2")
+# print("3")
+# print("1")
+# print("2")
+# print("3")
+# print("1")
+# print("2")
+# print("3")
+# print("1")
+# print("2")
+# print("3")
+
+
+# def print123():
+#   print("1")
+#   print("2")
+#   print("3")
+
+
+# print123()
+# print123()
+# print123()
+# print123()
+
+
+# def checkPassword(password):
+#   if password == "superpassword":
+#     print("CORECT")
+#   else:
+#     print("WRONG !!!")
+
+
+# password = input("Enter password: ")
+# checkPassword(password)
+
+
+# def multiply(num1, num2):
+#   return num1 * num2
+
+
+# print(multiply(10, 20))
+
+
+# Створіть функцію, яка приймає ім'я та вік,
+# а потім виводить повідомлення: "Hello, [ім'я]! You are [вік] yo."
+# def greet(name, age):
+#   isAdult = age >= 18
+#   print(f"Hello, {name}! You are {age} yo.")
+
+# greet("Artem", 18)
+
+# def isCouldBeDividedBy5(number):
+#   return number % 5 == 0
+
+
+# def getNumber():
+#   return int(input("Enter number: "))
+
+
+# def main():
+#   number = getNumber()
+#   print(isCouldBeDividedBy5(number))
+
+
+# main()
+
+
+# ДЗ Напишіть функцію, яка перевіряє, чи є число парним.
